eeefe38764d3c7547137bb2c81ef3f6b74aa3171
Thyago Silveira <83200721+Thyaguixx@users.noreply.github.com>
2024-09-12 08:50:42 -0300

    tratando dados do hugging face para API

---
+++ teste_dados.py
@@ -0,0 +1,13 @@
+from datasets import load_dataset
+
+dataset = load_dataset("ruanchaves/b2w-reviews01")
+dados = dataset['train']
+
+dados_pandas    = dados.to_pandas()
+dados_json      = dados_pandas.to_json(orient='records', indent=4, force_ascii=False)   #dados_pandas.to_json(orient='records', lines=True)
+
+with open('dados_dataset.json', 'w', encoding='utf-8') as f:
+    f.write(dados_json)
+
+
+
