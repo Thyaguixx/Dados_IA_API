25c7870dc4a6b6fa0765654d3ae7249be9be0602
Thyago Silveira <thyagogsilveira@gmail.com>
2024-09-19 12:21:59 -0300

    ajustes para ler o JSON com o pandas e remover os com ID de review duplicados

--- teste_chat.py
+++ teste_chat.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 from langchain.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI  # GoogleGenerativeAI
 from langchain_core.output_parsers import StrOutputParser
@@ -6,12 +7,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-with open('dados_dataset.json', 'r', encoding='utf-8') as f:
-    dados_json = json.load(f)
+# with open('dados_dataset.json', 'r', encoding='utf-8') as f:
+#     dados_json = json.load(f)
 
+dados_json      = pd.read_json("dados_dataset.json")
+dados_json      = dados_json.drop_duplicates(subset=['reviewer_id'])
 dados_limitados = dados_json[:100]
 
-contexto    = json.dumps(dados_limitados, ensure_ascii=False, indent=4)
+contexto        = dados_limitados.to_dict(orient="records")
+contexto_json   = json.dumps(contexto, ensure_ascii=False, indent=4)
 
 llm         = ChatGoogleGenerativeAI(model="gemini-pro")
 parser      = StrOutputParser()
@@ -26,12 +30,19 @@ chain = prompt | llm
 
 # Invocar o LLM com o contexto do JSON e a pergunta do usuário
 try:
-    resposta = chain.invoke({
-        "context": contexto,
-        "input_text": input("Digite a sua pergunta: ")
-    })
-    
-    resposta = parser.invoke(resposta)
-    print(resposta)
+    while True:
+        pergunta_usuario = input("Digite a sua pergunta (ou digite SAIR para parar): ")
+        
+        if pergunta_usuario.strip().upper() == 'SAIR':
+            print("Fechando o chat")
+            break
+        
+        resposta = chain.invoke({
+            "context": contexto_json,
+            "input_text": pergunta_usuario
+        })
+        
+        resposta = parser.invoke(resposta)
+        print(resposta)
 except Exception as e:
     print(f"Ocorreu um erro: {e}")
--- teste_dados.py
+++ teste_dados.py
@@ -4,10 +4,7 @@ dataset = load_dataset("ruanchaves/b2w-reviews01")
 dados = dataset['train']
 
 dados_pandas    = dados.to_pandas()
-dados_json      = dados_pandas.to_json(orient='records', indent=4, force_ascii=False)   #dados_pandas.to_json(orient='records', lines=True)
+dados_json      = dados_pandas.to_json(orient='records', indent=4, force_ascii=False)
 
 with open('dados_dataset.json', 'w', encoding='utf-8') as f:
     f.write(dados_json)
-
-
-
